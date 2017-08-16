b4a57d7cb1448211677566c0635a5ab2ddc11bfc
Mike Olie <aolie94@gmail.com>
2017-08-15 23:32:14 -0700

    added arrows on the side, changed heads on team, and cleaned up the routes along with app file

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -29,23 +29,6 @@ var router = function(nav) {
       .get(function(req, res) {
         res.render('editabout')
       });
-  // adminRouter.route('/signUp')
-  //     .post(function(req, res) {
-  //       var url = 'mongodb://admin:the_machine@ds034807.mlab.com:34807/the-machine';
-  //       mongodb.connect(url, function(err, db) {
-  //         var collection = db.collection('users');
-  //         var user = {
-  //           username: req.body.userName,
-  //           password: req.body.password
-  //         };
-  //         collection.insert(user, function(err, results) {
-  //           if (err) {console.log(err);}
-  //           req.login(results.ops[0], function() {
-  //             res.redirect('/auth/profile');
-  //           });
-  //         });
-  //       });
-  //     });
   return adminRouter;
 };
 module.exports = router;
