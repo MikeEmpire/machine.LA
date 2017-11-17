49ace92e2ca2e84cca0ab1cfe01c5e774fca568d
Mike Olie <aolie94@gmail.com>
2017-11-16 17:16:38 -0800

    edited splash page

--- public/js/machine.js
+++ public/js/machine.js
@@ -4,19 +4,13 @@ $(document).ready(function() {
 
   const splashPage = $("#splash-page");
 
-  $('#splash-text').mousemove(function(e) {
-    var rXP = (e.pageX - this.offsetLeft - $(this).width() / 2);
-    var rYP = (e.pageY - this.offsetTop - $(this).height() / 2);
-    $('#splash-text').css('text-shadow', + rYP / 10 + 'px ' + rXP / 80 + 'px rgba(227,6,19,.8), ' + rYP / 8 + 'px ' + rXP / 60 + 'px rgba(255,237,0,1), ' + rXP / 70 + 'px ' + rYP / 12 + 'px rgba(0,159,227,.7)');
-  });
-
   $("#enter-machine").click(function() {
     TweenMax.to(splashPage, 1, {autoAlpha:0});
   });
-
-  setTimeout(function() {
-    TweenMax.to(splashPage, 1, {autoAlpha:0});
-  }, 8000);
+  
+  // setTimeout(function() {
+  //   TweenMax.to(splashPage, 1, {autoAlpha:0});
+  // }, 8000);
 
   var controller = new ScrollMagic.Controller();
 
